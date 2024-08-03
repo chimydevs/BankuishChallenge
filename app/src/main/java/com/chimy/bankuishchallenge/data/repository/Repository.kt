e7e105517cf4c4a1039ecc09ad665c825ea5d985
michimurri <joselo.okumura@gmail.com>
2024-08-02 22:13:33 -0300

    ImplementRetrofit

---
+++ app/src/main/java/com/chimy/bankuishchallenge/data/repository/Repository.kt
@@ -0,0 +1,20 @@
+package com.chimy.bankuishchallenge.data.repository
+
+import com.chimy.bankuishchallenge.data.model.RepositoryModel
+import com.chimy.bankuishchallenge.data.network.GitHubApiService
+import retrofit2.http.Query
+
+//llamados a la api
+class Repository(private val apiService: GitHubApiService) {
+    suspend fun getRepository(query: String, perPage: Int, page: Int):List<RepositoryModel>{
+        return try { // llamada a a la api
+            val response = apiService.getRepositories(query,perPage, page)
+            response.items //retorna lista de repositorios
+        } catch (
+            e:Exception
+        ){
+            emptyList() //retorna una lista vacia si falla
+        }
+    }
+
+}
